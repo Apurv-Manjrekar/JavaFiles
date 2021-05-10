3363164019bbf456fbe4209533a54d0ec295581b
Apurv-Manjrekar <83988778+Apurv-Manjrekar@users.noreply.github.com>
2021-05-10 19:04:33 -0400

    Add files via upload

---
+++ MultTable/MultTable.java
@@ -0,0 +1,15 @@
+public class MultTable{ 
+    public static void main(){
+        int x;
+        int y;
+        for (x = 1; x <= 12; x++) {
+            for(y = 1; y <= 12; y++){
+
+                System.out.print(x * y + "\t");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
